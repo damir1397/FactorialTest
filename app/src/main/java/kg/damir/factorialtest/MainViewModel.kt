43ac4769ff359bad5466eaaacf659c7b571d9d28
damir <damir.danzo@gmail.com>
2023-11-27 00:11:53 +0600

    CoroutineScope and CoroutineContext

--- app/src/main/java/kg/damir/factorialtest/MainViewModel.kt
+++ app/src/main/java/kg/damir/factorialtest/MainViewModel.kt
@@ -1,16 +1,22 @@
 package kg.damir.factorialtest
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.math.BigInteger
 
 class MainViewModel : ViewModel() {
+    private val coroutineScope =
+        CoroutineScope(Dispatchers.Main + CoroutineName("My coroutine scope"))
 
     private val _state = MutableLiveData<State>()
     val state: LiveData<State>
@@ -23,22 +29,28 @@ class MainViewModel : ViewModel() {
             return
         }
 
-        viewModelScope.launch {
+        coroutineScope.launch {
             val number = value.toLong()
-            val result = factorial(number)
+            val result = withContext(Dispatchers.Default) {
+                factorial(number)
+            }
             _state.value = Factorial(value = result)
+
+            Log.d("MainViewModel",coroutineContext.toString())
         }
     }
 
-    private suspend fun factorial(number: Long): String {
-        return withContext(Dispatchers.Default) {
-            var result = BigInteger.ONE
-            for (i in 1..number) {
-                result = result.multiply(BigInteger.valueOf(i))
-            }
-            result.toString()
+    private fun factorial(number: Long): String {
+        var result = BigInteger.ONE
+        for (i in 1..number) {
+            result = result.multiply(BigInteger.valueOf(i))
         }
+        return result.toString()
+    }
 
+    override fun onCleared() {
+        super.onCleared()
+        coroutineScope.cancel()
     }
 }
 
